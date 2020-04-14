d778745765056966f532d9630ff1eaf5a1b3dfc8
Akinwole Adeola Abraham <ajunior488@gmail.com>
2020-04-14 17:48:06 +0100

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\LogAfterRequest::class,
-        \App\Http\Middleware\ApiLogger::class
+         
     ];
 
     /**
