1fcaae07670d329e35d20e8ac832553e11da7c62
Akinwole Adeola Abraham <ajunior488@gmail.com>
2020-04-14 16:18:26 +0100

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<table>
+@forelse ($apilogs as $key => $log)
+<tr>
+<td> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {{$log->method}}</td>
+<td> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$log->url}}</td>
+<td>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$log->response}}</td>
+<td>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$log->duration * 1000}}  ms</td>
+
+</tr>
+
+
+                                                @empty
+
+                  @endforelse
+
+</table>
+
